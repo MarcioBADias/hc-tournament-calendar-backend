6f7ae8da9a6f700010de529a5769cc7bd5416009
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-01-24 23:41:42 -0300

    ✅ Refactor do server.js para o app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const connectDB = require('./src/config/db')
+const tournamentRoutes = require('./src/routes/tournamentRoutes')
+const cors = require('cors')
+const PORT = process.env.PORT || 5000
+const app = express()
+
+connectDB()
+
+app.use(cors())
+app.use(express.json())
+app.use('api/tournaments', tournamentRoutes)
+
+
+app.listen(PORT, () => console.log(`Rodando servidor na poorta ${PORT}`))
--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-const express = require('express')
+const app = require('./app')
 const connectDB = require('./src/config/db')
-const cors = require('cors')
 const PORT = process.env.PORT || 5000
-const app = express()
 
 connectDB()
-
-app.use(cors())
-app.use(express.json())
-
 
 app.listen(PORT, () => console.log(`Rodando servidor na poorta ${PORT}`))
